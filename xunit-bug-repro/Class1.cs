c914a1b3a066bb468d6353b7f23f736ce5c44390
Uri Goldstein <uri.goldstein@gmail.com>
2019-07-08 17:39:51 +0300

    Code to repro visualstudio.xunit issue 154

---
+++ xunit-bug-repro/Class1.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+
+namespace xunit_bug_repro
+{
+    public class Class1
+    {
+        [Fact]
+        public void this_test_has_a_pretty_nameX21()
+        {
+            Assert.True(false);
+        }
+    }
+}
